2d64eee4e1dbbf30d3e1f1deb53f59e3cc176b70
m310851010 <310851010@qq.com>
2023-12-31 23:41:56 +0800

    组织机构

--- src/main/java/org/ssssssss/magicboot/utils/Base36Utils.java
+++ src/main/java/org/ssssssss/magicboot/utils/Base36Utils.java
@@ -1,5 +1,7 @@
 package org.ssssssss.magicboot.utils;
 
+import cn.hutool.core.util.StrUtil;
+
 import java.math.BigInteger;
 
 /**
@@ -12,8 +14,22 @@ public class Base36Utils {
      * @param base36Num 36进制的数
      * @return 返回 当前值 + 1
      */
-    public static String nextNum(String base36Num) {
+    public static String next(String base36Num) {
+
         return new BigInteger(base36Num, 36).add(BigInteger.ONE).toString(36);
     }
 
+    /**
+     * 获取下一个编码
+     * @param code 当前code,比如0000, 一下个code是0001
+     * @return
+     */
+    public static String nextCode(String code) {
+        int index = code.length() - 2;
+        String endCode  = code.substring(index);
+        String nextCode = next(endCode);
+        String pad = StrUtil.padPre(nextCode, 2, '0');
+        return code.substring(0, index).concat(pad);
+    }
 }
+
