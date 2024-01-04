42511db880ce3059f509ebd71b616c852180e9c6
m310851010 <310851010@qq.com>
2024-01-05 01:20:02 +0800

    服务器监控

--- src/main/java/org/ssssssss/magicboot/extension/XMagicMagicFunction.java
+++ src/main/java/org/ssssssss/magicboot/extension/XMagicMagicFunction.java
@@ -6,6 +6,8 @@ import org.ssssssss.magicapi.core.config.MagicFunction;
 import org.ssssssss.script.annotation.Comment;
 import org.ssssssss.script.annotation.Function;
 
+import java.lang.management.ManagementFactory;
+
 /**
  * 扩展函数
  */
@@ -20,5 +22,12 @@ public class XMagicMagicFunction implements MagicFunction {
     @Comment("使用Snowflake生成ID")
     public static long nextId() {
         return snowflake.nextId();
+    }
+
+
+    @Function
+    @Comment("获取Java虚拟机的开始运行时间")
+    public static long getJavaVMStartTime() {
+        return ManagementFactory.getRuntimeMXBean().getStartTime();
     }
 }
